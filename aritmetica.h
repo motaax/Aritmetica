69e492db76ad122640a6b8f9251a5c056200643a
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-03-05 21:26:00 -0300

    Add files via upload

---
+++ aritmetica.h
@@ -0,0 +1,12 @@
+int soma(int a, int b) {
+    return a + b;
+}
+int subtracao(int a, int b) {
+    return a - b;
+}
+int produto(int a, int b) {
+    return a * b;
+}
+int divisao(int a, int b) {
+    return a / b;
+}
